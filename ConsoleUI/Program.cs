7d3fe9531c3bf67e9ade2ca17f9d08704ff161bd
Thomas Hedegaard <thomashj93@gmail.com>
2020-08-17 21:59:03 +0200

    add proper scope for container lifecycle

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using DemoLibrary;
+﻿using Autofac;
+using DemoLibrary;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,12 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             var container = ContainerConfig.Configure();
-
+            // define scope of the instances created by the container 
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+                app.Run();
+            }
 
             Console.ReadLine();
         }
